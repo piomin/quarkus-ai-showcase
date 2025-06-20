f1964bdb38c0070b64fb23aa231665051074d937
piomin <piotr.minkowski@gmail.com>
2025-06-20 10:07:06 +0200

    Fix tests

--- src/test/java/pl/piomin/quarkus/ai/WalletControllerTest.java
+++ src/test/java/pl/piomin/quarkus/ai/WalletControllerTest.java
@@ -29,18 +29,6 @@ class WalletControllerTest {
     @Order(2)
     void testCalculateHighestWalletValue() {
         given()
-          .pathParam("days", 30)
-          .when().get("/wallet/highest-day/{days}")
-          .then().statusCode(200)
-                 .contentType(ContentType.TEXT)
-                 .body(notNullValue())
-                 .body(not(emptyString()));
-    }
-
-    @Test
-    @Order(3)
-    void testCalculateHighestWalletValueWithDifferentDays() {
-        given()
           .pathParam("days", 7)
           .when().get("/wallet/highest-day/{days}")
           .then().statusCode(200)
